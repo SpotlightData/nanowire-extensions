a3f3548379574d97a19ec449456d32c95745f8a1
whoatedacake <doitsimplefy@gmail.com>
2018-06-19 16:01:12 +0100

    improve naming

--- src/helpers/url.js
+++ src/helpers/url.js
@@ -1,11 +1,11 @@
-export function queryToString(query) {
+export function queryObjectToString(query) {
   return Object.keys(query)
     .reduce((url, key) => `${url}&${key}=${query[key]}`)
     .slice(1); // Removing the first & symbol
 }
 
 export function buildUrl(hasBase, baseUrl, query, url) {
-  const params = queryToString(query);
+  const params = queryObjectToString(query);
   const fullUrl = url + (params.length !== 0 ? `?${params}` : '');
   return hasBase ? baseUrl + fullUrl : fullUrl;
 }
