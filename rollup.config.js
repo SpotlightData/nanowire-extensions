a95131324f154e127a48b264a36eefc8bcb8fe7d
whoatedacake <doitsimplefy@gmail.com>
2018-06-19 13:50:47 +0100

    update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,12 @@
 /* Based on https://github.com/rollup/rollup-starter-lib/tree/babel */
-// import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import pkg from './package.json';
 
 export default [
   {
-    entry: 'src/index.js',
+    input: 'src/index.js',
     external: ['ms'],
-    targets: [{ dest: pkg.main, format: 'cjs' }, { dest: pkg.module, format: 'es' }],
+    output: [{ file: pkg.main, format: 'cjs' }, { file: pkg.module, format: 'es' }],
     plugins: [
       babel({
         exclude: ['node_modules/**'],
