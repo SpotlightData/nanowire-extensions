453848e7ee50b7caea5f7984a3f945fa468fc360
whoatedacake <doitsimplefy@gmail.com>
2018-06-19 15:35:25 +0100

    add tests for form helpers

--- src/helpers/form.js
+++ src/helpers/form.js
@@ -3,7 +3,7 @@
  * @param {Object} schema - Yup schema
  * @param {any} data - Data to be validated by the schema
  */
-export function validateYupSchema(schema, data, options) {
+export function validateSchema(schema, data, options) {
   let error;
   try {
     schema.validateSync(data, options);
