498557d6b6df0f7a0a1ee66e6f9ec2924e08208e
whoatedacake <doitsimplefy@gmail.com>
2018-06-20 13:12:50 +0100

    add react-jss peer dependency

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 import pkg from './package.json';
 
-const external = ['react', 'antd', 'moment'];
+const external = ['react', 'react-jss', 'antd', 'moment'];
 
 export default [
   {
