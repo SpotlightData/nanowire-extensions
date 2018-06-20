2d6a5cfbdd44538e6b12c5265f8bfff9fb094365
whoatedacake <doitsimplefy@gmail.com>
2018-06-20 11:00:45 +0100

    remove unused variables

--- tests/components/antd/Badge.test.js
+++ tests/components/antd/Badge.test.js
@@ -4,7 +4,7 @@ import { render } from 'react-testing-library';
 
 describe('components/antd/Badge', () => {
   it('should show counts over 99 by number', () => {
-    const { getByText, debug, container } = render(<Badge count={100} />);
+    const { container } = render(<Badge count={100} />);
     const elements = Array.from(container.getElementsByClassName('current'));
     expect(elements.map(t => t.textContent).join('')).toBe('100');
   });
