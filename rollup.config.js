1a5ffe034e5f49612b90fd791610b49acafdbbf1
whoatedacake <doitsimplefy@gmail.com>
2018-06-20 09:33:08 +0100

    Update bundler to solve cjs issues

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,21 @@
 /* Based on https://github.com/rollup/rollup-starter-lib/tree/babel */
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 import pkg from './package.json';
 
 export default [
   {
     input: 'src/index.js',
-    external: ['ms'],
+    external: ['react', 'antd'],
     output: [{ file: pkg.main, format: 'cjs' }, { file: pkg.module, format: 'es' }],
     plugins: [
+      resolve({
+        module: true,
+      }),
+      commonjs({
+        include: 'node_modules/**', // Default: undefined
+      }),
       babel({
         exclude: ['node_modules/**'],
       }),
