c25bc141b85c3e6281e02352d605e7450173bd48
whoatedacake <doitsimplefy@gmail.com>
2018-06-20 10:07:06 +0100

    Add hoc tests

--- src/components/hoc.js
+++ src/components/hoc.js
@@ -3,13 +3,22 @@ import hoistStatics from 'hoist-non-react-statics';
 
 export const strictComponent = (predicate, onFail) => Component => {
   class Wrapper extends PureComponent {
+    state = { checked: false };
     componentDidMount() {
       if (!predicate(this.props)) {
         onFail(this.props);
+      } else {
+        this.setState({ checked: true });
       }
     }
 
-    render = () => <Component {...this.props} />;
+    render = () => {
+      if (this.state.checked) {
+        return <Component {...this.props} />;
+      } else {
+        return null;
+      }
+    };
   }
   const componentName = Component.displayName || Component.name;
   Wrapper.WrappedComponent = Component;
