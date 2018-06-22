ab2d6214082d6dc34cae74b34316b54c5aa06fad
whoatedacake <doitsimplefy@gmail.com>
2018-06-22 16:39:30 +0100

    remove code comments

--- tests/components/uppy/FileTable.test.js
+++ tests/components/uppy/FileTable.test.js
@@ -62,28 +62,4 @@ describe('uppy/FileTable', () => {
       expect(uppy.getFiles().length).toBe(0);
     });
   });
-  // it('should register itself to uppy plugin list on mount', async () => {
-  //   const uppy = Uppy({ autoProceed: false });
-  //   render(
-  //     <UppyProvider uppy={uppy}>
-  //       <DragDropUploader onFail={() => false} />
-  //     </UppyProvider>
-  //   );
-  //   await waitForExpect(() => {
-  //     expect(uppy.plugins.acquirer[0].id).toBe('DragDrop');
-  //   });
-  // });
-
-  // it('should remove itself to uppy plugin list on unmount', async () => {
-  //   const uppy = Uppy({ autoProceed: false });
-  //   const { unmount } = render(
-  //     <UppyProvider uppy={uppy}>
-  //       <DragDropUploader onFail={() => false} />
-  //     </UppyProvider>
-  //   );
-  //   unmount();
-  //   await waitForExpect(() => {
-  //     expect(uppy.plugins.acquirer.length).toBe(0);
-  //   });
-  // });
 });
