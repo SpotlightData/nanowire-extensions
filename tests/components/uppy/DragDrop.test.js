566f820bfc4e426fec1743e82930f3534e3e05f5
whoatedacake <doitsimplefy@gmail.com>
2018-06-22 15:35:46 +0100

    drag drop tests

--- tests/components/uppy/DragDrop.test.js
+++ tests/components/uppy/DragDrop.test.js
@@ -4,8 +4,53 @@ import { render, fireEvent } from 'react-testing-library';
 import Uppy from 'uppy/lib/core';
 import waitForExpect from 'wait-for-expect';
 
+function removeFromArray(array, item) {
+  var index = array.indexOf(item);
+
+  if (index >= 0) {
+    array.splice(index, 1);
+  }
+}
+
+var DataTransfer = function() {
+  this.dataByFormat = {};
+
+  this.dropEffect = 'none';
+  this.effectAllowed = 'all';
+  this.files = [];
+  this.types = [];
+};
+
+DataTransfer.prototype.clearData = function(dataFormat) {
+  if (dataFormat) {
+    delete this.dataByFormat[dataFormat];
+    removeFromArray(this.types, dataFormat);
+  } else {
+    this.dataByFormat = {};
+    this.types = [];
+  }
+};
+
+DataTransfer.prototype.getData = function(dataFormat) {
+  return this.dataByFormat[dataFormat];
+};
+
+DataTransfer.prototype.setData = function(dataFormat, data) {
+  this.dataByFormat[dataFormat] = data;
+
+  if (this.types.indexOf(dataFormat) < 0) {
+    this.types.push(dataFormat);
+  }
+
+  return true;
+};
+
+DataTransfer.prototype.setDragImage = function() {
+  // don't do anything (the stub just makes sure there is no error thrown if someone tries to call the method)
+};
+
 describe('uppy/DragDrop', () => {
-  it('should provide a dropArea', () => {
+  it('should provide a drop area', () => {
     const uppy = Uppy({ autoProceed: false });
     const { queryByText } = render(
       <UppyProvider uppy={uppy}>
@@ -40,6 +85,36 @@ describe('uppy/DragDrop', () => {
     });
   });
 
+  it('should call onFail function for files with unaccepted extensions', async () => {
+    const uppy = Uppy({ autoProceed: false });
+    const { container, debug } = render(
+      <UppyProvider uppy={uppy}>
+        <DragDropUploader onFail={() => false} />
+      </UppyProvider>
+    );
+    const input = container.querySelector('.uppy-DragDrop-input');
+    // console.log(input.onchange(new Event('drop', {
+    //     type: 'drop',
+    //     dataTransfer: new DataTransfer(),
+    //     bubbles: true,
+    //     cancelable: false,
+    //   })));
+    fireEvent.drop(input, { dataTransfer: new DataTransfer() });
+    // console.log(queryByText('Drop files here or'));
+    // fireEvent(
+    //   queryByText('Drop files here or'),
+    //   new Event('drop', {
+    //     type: 'drop',
+    //     dataTransfer: new DataTransfer(),
+    //     bubbles: true,
+    //     cancelable: false,
+    //   })
+    // );
+    // unmount();
+    // await waitForExpect(() => {
+    //   expect(uppy.plugins.acquirer.length).toBe(0);
+    // });
+  });
   // it('should register to uppy plugin list', () => {
   //   const uppy = Uppy({
   //     autoProceed: false,
