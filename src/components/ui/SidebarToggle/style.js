34c47be0888685afd2233d9046050f39cb3f1829
whoatedacake <doitsimplefy@gmail.com>
2018-06-22 10:37:57 +0100

    Sidebar component

---
+++ src/components/ui/SidebarToggle/style.js
@@ -0,0 +1,9 @@
+export default {
+  root: {
+    fontSize: '20px',
+    margin: 'auto 1em',
+    marginLeft: '0',
+    cursor: 'pointer',
+    userSelect: 'none',
+  },
+};
--- src/components/ui/main.js
+++ src/components/ui/main.js
@@ -1,2 +1,3 @@
 export * from './Loading';
 export * from './ProgressBar';
+export * from './SidebarToggle/index';
