7919f65619a316184fca1229af2f0ae2e529b36d
whoatedacake <doitsimplefy@gmail.com>
2018-06-25 09:11:31 +0100

    Either helper

---
+++ tests/helpers/Either.test.js
@@ -0,0 +1,36 @@
+import { Either } from '@spotlightdata/nanowire-extensions';
+
+describe('helpers/Either', () => {
+  it('create correct type from arguments', () => {
+    expect(new Either(true, false).isLeft).toBe(true);
+    expect(new Either(null, []).isLeft).toBe(false);
+  });
+
+  it('should map functions over arguments correctly', () => {
+    expect(
+      Either.from(1, false)
+        .leftMap(a => a + 1)
+        .takeLeft()
+    ).toBe(2);
+    expect(
+      Either.from(null, 1)
+        .rightMap(a => a + 1)
+        .takeRight()
+    ).toBe(2);
+  });
+
+  it('should not change the type when using map', () => {
+    const leftCall = jest.fn();
+    const rightCall = jest.fn();
+
+    Either.from(1, false)
+      .leftMap(a => a + 1)
+      .rightMap(rightCall);
+    Either.from(false, 1)
+      .rightMap(a => a + 1)
+      .leftMap(leftCall);
+
+    expect(leftCall.mock.calls.length).toBe(0);
+    expect(rightCall.mock.calls.length).toBe(0);
+  });
+});
