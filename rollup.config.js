8ad4af6378478658cbda605d5c434922711fc89a
Augustinas <ajokubauskass@gmail.com>
2018-07-07 11:35:46 +0300

    update build configuration

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 import pkg from './package.json';
 
-const external = ['react', 'react-jss', 'antd', 'moment', 'ramda'];
+const external = ['react', 'react-jss', 'antd', 'moment', 'ramda', 'shortid'];
 
 export default [
   {
