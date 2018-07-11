abb8ac041ae94a719ea4b4736e4473900b4602a0
whoatedacake <doitsimpelfy@gmail.com>
2018-07-11 20:08:14 +0300

    pass props from text components to antd ones

--- src/components/form/TextArea/index.js
+++ src/components/form/TextArea/index.js
@@ -5,11 +5,11 @@ import { Input } from 'antd';
 const { TextArea: TextAreaComp } = Input;
 
 export const TextArea = props => {
-  const { input, rows, meta, placeholder } = props;
+  const { input, rows, meta, render, ...rest } = props;
   const error = meta.error || meta.submitError;
   return (
     <div className="field">
-      <TextAreaComp rows={rows} {...input} placeholder={placeholder} />
+      <TextAreaComp rows={rows} {...input} {...rest} />
       {error && <span className="field-error">{error}</span>}
     </div>
   );
