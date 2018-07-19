a38719d6721edeafd90f7396577396dbebac31ae
WhoAteDaCake <doitsimpelfy@gmail.com>
2018-07-19 17:44:47 +0300

    added generateColumns utility

--- src/helpers/table.js
+++ src/helpers/table.js
@@ -1,4 +1,4 @@
-import { propOr, curry } from 'ramda';
+import { propOr, curry, append } from 'ramda';
 
 function stringSort(a, b) {
   return a.localeCompare(b);
@@ -9,3 +9,16 @@ export const propSort = curry((key, a, b) => {
   const bVal = propOr('', key, b);
   return typeof aVal === 'string' ? stringSort(aVal, bVal) : aVal - bVal;
 });
+
+export function generateColumns(columns) {
+  return isExpanded => {
+    const list = typeof columns === 'function' ? columns(isExpanded) : columns;
+    return list.reduce((items, entry) => {
+      if (!isExpanded && entry.expandedOnly) {
+        return items;
+      }
+      const { expandedOnly, ...rest } = entry;
+      return append(rest, items);
+    }, []);
+  };
+}
