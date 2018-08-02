0c8ed379bfc59a5fbd20a2210b228219e4ff4c28
WhoAteDaCake <doitsimplefy@gmail.com>
2018-08-02 11:27:54 +0100

    move bytesToReadable function to helpers

--- src/uppy/FileTable/columns.js
+++ src/uppy/FileTable/columns.js
@@ -4,20 +4,7 @@ import { Button } from 'antd';
 import shortid from 'shortid';
 import { defaultTimeFormat } from '../../helpers/time';
 import { propSort } from '../../helpers/table';
-
-const units = ['bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-
-function bytesToReadable(x) {
-  let l = 0;
-  let n = parseInt(x, 10) || 0;
-
-  while (n >= 1024) {
-    n /= 1024;
-    l += 1;
-  }
-  const rounded = n.toFixed(n >= 10 || l < 1 ? 0 : 1);
-  return `${rounded} ${units[l]}`;
-}
+import { bytesToReadable } from '../../helpers/shared';
 
 export default deleteHandler => [
   {
@@ -39,16 +26,11 @@ export default deleteHandler => [
   {
     title: 'Last Modified',
     dataIndex: 'data.lastModified',
-    render: (text, row, index) => ({
-      children: <span key={shortid.generate()}>{defaultTimeFormat(row.data.lastModified)}</span>,
-    }),
+    render: defaultTimeFormat,
   },
   {
     title: '',
     dataIndex: 'id',
-    className: 'open-link',
-    render: id => ({
-      children: <Button onClick={deleteHandler(id)}>Remove</Button>,
-    }),
+    render: id => <Button onClick={deleteHandler(id)}>Remove</Button>,
   },
 ];
