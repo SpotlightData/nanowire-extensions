bef004dbbcfd003ce9e60372e6b759366140b12d
WhoAteDaCake <doitsimplefy@gmail.com>
2018-09-05 12:00:21 +0100

    add collapse component

---
+++ src/components/antd/Collapse/index.js
@@ -0,0 +1,75 @@
+import React, { PureComponent } from 'react';
+import { bool, node, func, number } from 'prop-types';
+
+import AnimateHeight from 'react-animate-height';
+
+import injectSheets from 'react-jss';
+import cn from 'classnames';
+
+import { style, height } from './style';
+
+const CollapsiblePanelBare = props => {
+  const {
+    children,
+    classes,
+    className,
+    collapsed,
+    header,
+    extraContent,
+    onClick,
+    duration,
+    headerRender,
+    ...rest
+  } = props;
+  const rootClass = cn({
+    [classes.root]: true,
+    [className || '']: true,
+    collapsed,
+  });
+  const arrowClass = cn({
+    [classes.arrow]: true,
+    right: collapsed,
+    down: !collapsed,
+  });
+  const button = (
+    <div role="button" onClick={() => onClick(!collapsed)}>
+      <i className={arrowClass} />
+      {header}
+    </div>
+  );
+  return (
+    <div className={rootClass} {...rest}>
+      {headerRender ? (
+        headerRender({ className: classes.header, button, extraContent })
+      ) : (
+        <div className={classes.header}>
+          {button}
+          <div>{extraContent}</div>
+        </div>
+      )}
+      <AnimateHeight duration={duration} height={collapsed ? 0 : 'auto'}>
+        <div className={classes.content}>{collapsed ? null : children}</div>
+      </AnimateHeight>
+    </div>
+  );
+};
+
+CollapsiblePanelBare.propTypes = {
+  collapsed: bool.isRequired,
+  onClick: func.isRequired,
+  extraContent: node,
+  header: node,
+  duration: number,
+  headerRender: func,
+};
+
+CollapsiblePanelBare.defaultProps = {
+  header: null,
+  extraContent: null,
+  duration: 400,
+  headerRender: null,
+};
+
+CollapsiblePanelBare.displayName = 'CollapsiblePanelBare';
+
+export const CollapsiblePanel = injectSheets(style)(CollapsiblePanelBare);
---
+++ src/components/antd/Collapse/style.js
@@ -0,0 +1,49 @@
+export const height = 46;
+
+export const style = {
+  root: {
+    width: '100%',
+    borderTop: '1px solid rgb(217, 217, 217)',
+    '&:first-child': {
+      borderTop: 'none',
+    },
+    '&.collapsed': {
+      height,
+    },
+  },
+  header: {
+    width: '100%',
+    padding: '0.5em',
+    fontSize: '15px',
+    height,
+    '& > div': {
+      '&:first-child': {
+        float: 'left',
+        cursor: 'pointer',
+        userSelect: 'none',
+        marginTop: `calc((${height}px - 1em - 22px) / 2)`,
+      },
+      '&:last-child': {
+        float: 'right',
+      },
+    },
+  },
+  arrow: {
+    border: 'solid rgba(0,0,0,.85)',
+    borderWidth: '0 1px 1px 0',
+    display: 'inline-block',
+    padding: '3px',
+    transition: 'transform 0.1s linear',
+    marginRight: '1em',
+    marginBottom: '2px',
+    '&.right': {
+      transform: 'rotate(-45deg)',
+    },
+    '&.down': {
+      transform: 'rotate(45deg)',
+    },
+  },
+  content: {
+    padding: '0.5em',
+  },
+};
