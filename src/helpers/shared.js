6071b40623d6426fbb3efef7b860a50ad8d689a6
whoatedacake <doitsimplefy@gmail.com>
2018-09-13 19:37:18 +0100

    move helpers to internal functions

--- src/helpers/shared.js
+++
@@ -1,2 +0,0 @@
-export const flatten = list =>
-  list.reduce((a, b) => a.concat(Array.isArray(b) ? flatten(b) : b), []);
--- src/helpers/table.js
+++ src/helpers/table.js
@@ -1,4 +1,4 @@
-import { propOr, curry, append } from 'ramda';
+import * as R from 'ramda';
 import { keyToAccessor } from '../internal/functions';
 
 function stringSort(a, b) {
@@ -22,21 +22,25 @@ export function generateColumns(columns) {
         return items;
       }
       const { expandedOnly, ...rest } = entry;
-      return append(rest, items);
+      return R.append(rest, items);
     }, []);
   };
 }
 
 export const SIZE_UNITS = ['bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
 
-export function bytesToReadable(x) {
-  let l = 0;
-  let n = parseInt(x, 10) || 0;
+export function bytesToReadableBare(units) {
+  return x => {
+    let l = 0;
+    let n = parseInt(x, 10) || 0;
 
-  while (n >= 1024) {
-    n /= 1024;
-    l += 1;
-  }
-  const rounded = n.toFixed(n >= 10 || l < 1 ? 0 : 1);
-  return `${rounded} ${SIZE_UNITS[l]}`;
+    while (n >= 1024) {
+      n /= 1024;
+      l += 1;
+    }
+    const rounded = n.toFixed(n >= 10 || l < 1 ? 0 : 1);
+    return `${rounded} ${units[l]}`;
+  };
 }
+
+export const bytesToReadable = bytesToReadableBare(SIZE_UNITS);
