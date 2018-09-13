2b451fb6bc2e79946c90dc42ea9247c9b846a520
WhoAteDaCake <doitsimplefy@gmail.com>
2018-09-13 08:53:26 +0100

    update error formatting

--- demo/index.js
+++ demo/index.js
@@ -1,6 +1,10 @@
 import { AjaxObservable } from '../src/internal/ajax';
+import { extractBackendError } from '../src';
 
 AjaxObservable.create({
   method: 'get',
   url: 'https://jsonplaceholder.typicode.com/todos/a',
-}).subscribe(console.log);
+}).subscribe(([error, resp]) => {
+  const message = extractBackendError('default')(error);
+  console.log(message);
+});
--- src/helpers/error.js
+++ src/helpers/error.js
@@ -5,7 +5,8 @@ export const extractErrorText = (error, response) => {
     return (
       pathOr(false, ['data', 'message'], response) ||
       pathOr(false, ['data', 'description'], response) ||
-      pathOr(false, ['data', 'error'], response)
+      pathOr(false, ['data', 'error'], response) ||
+      error.message
     );
   }
   return error.message || JSON.stringify(data);
@@ -17,25 +18,6 @@ export const extractBackendError = message => error => {
     message,
     description: extractErrorText(error, response),
     type: pathOr(false, ['data', 'type'], response),
-    raw: error.response,
+    raw: { ...response.data, status: response.status },
   };
 };
-// export const extractErrorText = ({ response, statusText, data }) => {
-//   if (response) {
-//     return (
-//       pathOr(false, ['message'], response) ||
-//       pathOr(false, ['description'], response) ||
-//       pathOr(false, ['error'], response)
-//     );
-//   }
-//   return statusText || JSON.stringify(data);
-// };
-
-// export const extractBackendError = message => error => {
-//   return {
-//     message,
-//     description: extractErrorText(error),
-//     type: pathOr(false, ['response', 'type'], error),
-//     raw: error,
-//   };
-// };
