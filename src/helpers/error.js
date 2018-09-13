fa00c31485c1c88b1476ad5d1a548d41773e27eb
WhoAteDaCake <doitsimplefy@gmail.com>
2018-09-13 08:44:06 +0100

    update error extraction

--- src/helpers/error.js
+++ src/helpers/error.js
@@ -1,21 +1,41 @@
 import { pathOr } from 'ramda';
 
-export const extractErrorText = ({ response, statusText, data }) => {
+export const extractErrorText = (error, response) => {
   if (response) {
     return (
-      pathOr(false, ['message'], response) ||
-      pathOr(false, ['description'], response) ||
-      pathOr(false, ['error'], response)
+      pathOr(false, ['data', 'message'], response) ||
+      pathOr(false, ['data', 'description'], response) ||
+      pathOr(false, ['data', 'error'], response)
     );
   }
-  return statusText || JSON.stringify(data);
+  return error.message || JSON.stringify(data);
 };
 
 export const extractBackendError = message => error => {
+  const { response } = error;
   return {
     message,
-    description: extractErrorText(error),
-    type: pathOr(false, ['response', 'type'], error),
-    raw: error,
+    description: extractErrorText(error, response),
+    type: pathOr(false, ['data', 'type'], response),
+    raw: error.response,
   };
 };
+// export const extractErrorText = ({ response, statusText, data }) => {
+//   if (response) {
+//     return (
+//       pathOr(false, ['message'], response) ||
+//       pathOr(false, ['description'], response) ||
+//       pathOr(false, ['error'], response)
+//     );
+//   }
+//   return statusText || JSON.stringify(data);
+// };
+
+// export const extractBackendError = message => error => {
+//   return {
+//     message,
+//     description: extractErrorText(error),
+//     type: pathOr(false, ['response', 'type'], error),
+//     raw: error,
+//   };
+// };
