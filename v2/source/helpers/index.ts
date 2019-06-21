8ab0a5bc98684fac5795000b543a22b35ce2447a
WhoAteDaCake <doitsimplefy@gmail.com>
2019-06-21 11:43:57 +0100

    time helpers

--- v2/source/helpers/index.ts
+++ v2/source/helpers/index.ts
@@ -3,3 +3,6 @@ export * from './error';
 export * from './Result';
 export * from './number';
 export * from './schema';
+export * from './file';
+export * from './functions';
+export * from './time';
