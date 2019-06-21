fe59d8abd17ed77851d45cac8b59c148ad5c7443
WhoAteDaCake <doitsimplefy@gmail.com>
2019-06-21 13:51:03 +0100

    object helpers

--- v2/source/helpers/object.ts
+++ v2/source/helpers/object.ts
@@ -3,12 +3,12 @@ import { Dictionary } from '../interfaces';
 
 export function filterObject<T>(
   object: Dictionary<T>,
-  predicate: (entry: T) => boolean
+  predicate: (key: string, entry: T) => boolean
 ): Dictionary<T> {
   return R.pipe(
     R.toPairs,
     R.reduce((acc: Dictionary<T>, [key, value]: [string, T]) => {
-      if (predicate(value)) {
+      if (predicate(key, value)) {
         acc[key] = value;
       }
       return acc;
