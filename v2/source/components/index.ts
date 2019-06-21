2af82aab711f11772829225efb08487ca75f5f39
WhoAteDaCake <doitsimplefy@gmail.com>
2019-06-21 16:06:33 +0100

    DatePicker

--- v2/source/components/index.ts
+++ v2/source/components/index.ts
@@ -2,5 +2,6 @@ export * from './ValidateSaved';
 export * from './ContentPopconfirm';
 export * from './SimpleProgressBar';
 export * from './Badge';
+export * from './DatePicker';
 
 export * from './strictComponent';
