1897b5e438f0c7e122c9a7eb706a0aa4ac8d944a
WhoAteDaCake <doitsimplefy@gmail.com>
2019-06-24 14:37:04 +0100

    switch demo

--- v2/demo/Form.tsx
+++ v2/demo/Form.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Form, Field } from 'react-final-form';
-import { Checkbox, TextField } from '../source';
+import { Checkbox, TextField, Switch } from '../source';
 
 export class FormDemo extends React.PureComponent {
   render() {
@@ -12,6 +12,7 @@ export class FormDemo extends React.PureComponent {
             <React.Fragment>
               <Field name="checkbox" render={p => <Checkbox {...p} label="Toggle" />} />
               <Field name="text" render={p => <TextField {...p} />} />
+              <Field name="switch" render={p => <Switch {...p} />} />
             </React.Fragment>
           );
         }}
