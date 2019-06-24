a8711e3416b487634cee9d6a1788ead0809563e4
WhoAteDaCake <doitsimplefy@gmail.com>
2019-06-24 10:00:17 +0100

    make sure timeline item is dictionary

--- v2/source/components/Timeline/index.tsx
+++ v2/source/components/Timeline/index.tsx
@@ -1,13 +1,18 @@
 import * as React from 'react';
+import * as R from 'ramda';
 import { Timeline as AntTimeline } from 'antd';
+import { Dictionary } from 'v2/source/interfaces';
 
 export interface TimelineProps<T> {
   accessor: string | ((item: T) => string);
   items: T[];
 }
 
-export function Timeline<T>({ items, accessor }: TimelineProps<T>): React.ReactElement {
-  const getter = typeof accessor === 'string' ? (a: T) => a[accessor] : accessor;
+export function Timeline<T extends Dictionary<T>>({
+  items,
+  accessor,
+}: TimelineProps<T>): React.ReactElement {
+  const getter = typeof accessor === 'string' ? R.prop(accessor) : accessor;
 
   function line(entry: T, index: number) {
     const text = getter(entry);
