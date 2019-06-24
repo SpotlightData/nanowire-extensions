a01f0b9f804312af1ca8492e7340e9f0ed82c79d
WhoAteDaCake <doitsimplefy@gmail.com>
2019-06-24 14:25:06 +0100

    add textfield to form demo

--- v2/demo/Form.tsx
+++ v2/demo/Form.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Form, Field } from 'react-final-form';
-import { Checkbox } from '../source';
+import { Checkbox, TextField } from '../source';
 
 export class FormDemo extends React.PureComponent {
   render() {
@@ -11,6 +11,7 @@ export class FormDemo extends React.PureComponent {
           return (
             <React.Fragment>
               <Field name="checkbox" render={p => <Checkbox {...p} label="Toggle" />} />
+              <Field name="text" render={p => <TextField {...p} />} />
             </React.Fragment>
           );
         }}
