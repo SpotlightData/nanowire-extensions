835791be544237f030b11d204ce22532e387a192
WhoAteDaCake <doitsimplefy@gmail.com>
2019-06-24 12:58:24 +0100

    checkbox

---
+++ v2/source/components/form/Checkbox/index.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+
+import { FieldRenderProps } from 'react-final-form';
+import { Checkbox as CheckboxInput } from 'antd';
+import { CheckboxChangeEvent } from 'antd/lib/checkbox';
+
+export interface CheckboxProps extends FieldRenderProps<boolean, HTMLInputElement> {
+  defaultValue?: boolean;
+  label: string;
+}
+
+export class Checkbox extends React.Component<CheckboxProps> {
+  componentWillMount() {
+    const { defaultValue, input } = this.props;
+    if (defaultValue) {
+      input.onChange(defaultValue);
+    }
+  }
+
+  onChange = (e: CheckboxChangeEvent) => {
+    this.props.input.onChange(e.target.checked);
+  };
+
+  render() {
+    const { onChange, ...rest } = this.props.input;
+    return (
+      <CheckboxInput {...rest} onChange={this.onChange}>
+        {this.props.label}
+      </CheckboxInput>
+    );
+  }
+}
+
+// @ts-ignore
+Checkbox.defaultProps = {
+  defaultValue: false,
+};
--- v2/source/components/index.ts
+++ v2/source/components/index.ts
@@ -1,2 +1,3 @@
 export * from './antd';
+export * from './form';
 export * from './strictComponent';
