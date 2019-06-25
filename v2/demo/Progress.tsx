65d4d7aa4ce7c7142cac721aef7dca5663d6e666
WhoAteDaCake <doitsimplefy@gmail.com>
2019-06-25 13:00:58 +0100

    Use redux in one of the demos

--- v2/demo/Progress.tsx
+++ v2/demo/Progress.tsx
@@ -1,22 +1,36 @@
 import * as React from 'react';
 import { SimpleProgressBar } from '../source';
+import { Row } from 'antd';
+import { store, CASES, R1State } from './redux';
+
+const r1state = () => store.getState()['r1state'] as R1State;
 
 export class Progress extends React.PureComponent {
-  state = { failed: 10, success: 0, total: 200 };
+  state = r1state();
 
   bloop = () => {
-    const { success, failed, total } = this.state;
-    if (success + failed !== total) {
-      this.setState({ success: success + 1 });
-      setTimeout(this.bloop, 10);
-    }
+    store.dispatch({ type: CASES.INCREASE, progress: 1 });
+    this.setState(r1state(), () => setTimeout(this.bloop, 50));
   };
 
   componentDidMount() {
+    this.reset();
     this.bloop();
   }
 
+  reset() {
+    store.dispatch({ type: CASES.SET, failed: 10, total: 200, progress: 0 });
+  }
+
   render() {
-    return <SimpleProgressBar {...this.state} />;
+    const { failed, progress, total } = this.state;
+    return (
+      <Row type="flex">
+        <div style={{ width: '90%' }}>
+          <SimpleProgressBar failed={failed} total={total} success={progress} />
+        </div>
+        <button onClick={() => this.reset()}>RESET</button>
+      </Row>
+    );
   }
 }
