4649147438ace9157ddd863e161c07b71a66da62
WhoAteDaCake <doitsimplefy@gmail.com>
2019-06-25 09:22:07 +0100

    add single selector scroll demo

--- v2/demo/Form.tsx
+++ v2/demo/Form.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Form, Field } from 'react-final-form';
-import { Checkbox, TextField, Switch, Dropdown } from '../source';
+import { Checkbox, TextField, Switch, Dropdown, ScrollListFieldSingle } from '../source';
 
 export const options = {
   FILE: { text: 'Local files' },
@@ -20,14 +20,25 @@ export class FormDemo extends React.PureComponent {
               <Field name="checkbox" render={p => <Checkbox {...p} label="Toggle" />} />
               <Field name="text" render={p => <TextField {...p} />} />
               <Field name="switch" render={p => <Switch {...p} />} />
-              {/* <Field
+              <Field
                 name="switch"
                 render={p => <Dropdown {...p} options={options} label="Options" />}
-              /> */}
+              />
               <Field
                 name="switch-def"
                 render={p => (
                   <Dropdown {...p} options={options} label="Options" defaultOption="FILE" />
+                )}
+              />
+              <Field
+                name="scroll-single"
+                render={p => (
+                  <ScrollListFieldSingle
+                    {...p}
+                    items={options}
+                    accessor={a => a.text}
+                    height={100}
+                  />
                 )}
               />
             </React.Fragment>
