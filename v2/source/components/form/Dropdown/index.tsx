184378049ca3dc59f4300e74c296fd6ef1c363f0
WhoAteDaCake <doitsimplefy@gmail.com>
2019-06-26 14:25:54 +0100

    Add ability to override dropdown value before and after passing

--- v2/source/components/form/Dropdown/index.tsx
+++ v2/source/components/form/Dropdown/index.tsx
@@ -6,45 +6,66 @@ import { FieldRenderProps } from 'react-final-form';
 import { Dictionary } from '../../../interfaces';
 import { FieldWrapper } from '../FieldWrapper';
 import { ClickParam } from 'antd/lib/menu';
+import { ValueOf } from 'ts-essentials';
 
 export interface DropdownEntry {
   text: string;
   disabled?: boolean;
 }
 
-export interface DropdownProps<T> extends FieldRenderProps<string, HTMLTextAreaElement> {
+export interface DropdownProps<T, O> extends FieldRenderProps<O, HTMLTextAreaElement> {
   defaultOption?: keyof T;
   maxWidth?: number;
   options: T;
   label: string;
+  preTransform?: (before: O) => string;
+  postTransform?: (after: string) => O;
 }
 
-export class Dropdown<T extends Dictionary<DropdownEntry>> extends React.Component<
-  DropdownProps<T>
+export class Dropdown<T extends Dictionary<DropdownEntry>, O> extends React.Component<
+  DropdownProps<T, O>
 > {
   static defaultProps = {
     accessor: 'key',
     maxWidth: 100,
   };
 
+  private _getValue() {
+    const { input, preTransform } = this.props;
+    if (preTransform) {
+      return preTransform(input.value);
+    }
+    return (input.value as any) as string;
+  }
+
+  private _setValue(data: string) {
+    const { input, postTransform } = this.props;
+    let value: O;
+    if (postTransform) {
+      value = postTransform(data);
+    } else {
+      value = (data as any) as O;
+    }
+    input.onChange(value);
+  }
+
   componentDidMount() {
     const { input, defaultOption } = this.props;
     if (defaultOption) {
       // Need to delay, otherwise it doesn't get updated
       setTimeout(() => {
-        input.onChange(defaultOption);
+        this._setValue(defaultOption as string);
       }, 0);
     }
   }
 
   handleRequestClose = (req: ClickParam) => {
-    const { input } = this.props;
-    input.onChange(req.key);
+    this._setValue(req.key);
   };
 
   render() {
-    const { options, input, label, meta, maxWidth } = this.props;
-    const selected = options[input.value];
+    const { options, label, meta, maxWidth } = this.props;
+    const selected = options[this._getValue()];
 
     const menu = (
       <Menu onClick={this.handleRequestClose} style={{ maxWidth: `${maxWidth}px` }}>
