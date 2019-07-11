b572408e58314ff8d07d0565dda69d28fe1c8574
WhoAteDaCake <doitsimplefy@gmail.com>
2019-07-11 10:23:51 +0100

    Fix switch to use boolean props

--- v2/source/components/form/Switch/index.tsx
+++ v2/source/components/form/Switch/index.tsx
@@ -5,26 +5,18 @@ import { FieldRenderProps } from 'react-final-form';
 import { FieldError } from '../FieldError';
 import { FieldWrapper } from '../FieldWrapper';
 
-export interface SwitchProps extends FieldRenderProps<string, HTMLInputElement> {}
+export interface SwitchProps extends FieldRenderProps<boolean, HTMLInputElement> {}
 
 /**
  * Use String wrapper for input.value because react-final-form
  * can pass empty string as value in some cases
  */
 export class Switch extends React.Component<SwitchProps> {
-  componentWillMount() {
-    const { input } = this.props;
-    // Need to delay, otherwise it doesn't get updated
-    setTimeout(() => {
-      input.onChange(String(input.value) === 'true');
-    }, 0);
-  }
-
   render() {
     const { input, meta } = this.props;
     return (
       <FieldWrapper>
-        <AntSwitch checked={String(input.value) === 'true'} onChange={input.onChange} />
+        <AntSwitch checked={input.value} onChange={(checked: boolean) => input.onChange(checked)} />
         <FieldError meta={meta} />
       </FieldWrapper>
     );
