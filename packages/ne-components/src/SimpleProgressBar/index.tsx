66a43b68e96f8428b3296515b8dc34fec94574d8
WhoAteDaCake <doitsimplefy@gmail.com>
2019-09-25 17:34:55 +0100

    (feat) move old antd components over

---
+++ packages/ne-components/src/SimpleProgressBar/index.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import injectSheet from 'react-jss';
+
+export interface SimpleProgressBarClasses {
+  container: string;
+  success: string;
+  failed: string;
+}
+
+export interface SimpleProgressBarBareProps {
+  total: number;
+  success: number;
+  failed: number;
+}
+
+export interface SimpleProgressBarProps extends SimpleProgressBarBareProps {
+  classes: SimpleProgressBarClasses;
+}
+
+const styles = {
+  container: {
+    backgroundColor: '#f5f5f5',
+    height: '8px',
+    borderRadius: '100px',
+    display: 'flex',
+    overflow: 'hidden',
+  },
+  success: (props: SimpleProgressBarBareProps) => {
+    const passed = (props.success / props.total) * 100;
+    return { backgroundColor: '#52c41a', width: `${passed}%` };
+  },
+  failed(props: SimpleProgressBarBareProps) {
+    const passed = (props.failed / props.total) * 100;
+    return { backgroundColor: '#f5222d', width: `${passed}%` };
+  },
+};
+
+const SimpleProgressBarBare: React.FC<SimpleProgressBarProps> = ({ classes }) => {
+  return (
+    <div className={classes.container}>
+      <span className={classes.success} />
+      <span className={classes.failed} />
+    </div>
+  );
+};
+
+// @ts-ignore
+export const SimpleProgressBar = injectSheet<string, object, SimpleProgressBarBareProps>(styles)(
+  SimpleProgressBarBare
+);
---
+++ packages/ne-components/src/Timeline/index.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import * as R from 'ramda';
+import { Timeline as AntTimeline } from 'antd';
+import { Dictionary } from 'ts-essentials';
+
+export interface TimelineProps<T> {
+  accessor: string | ((item: T) => string);
+  items: T[];
+}
+
+export function Timeline<T extends Dictionary<T>>({
+  items,
+  accessor,
+}: TimelineProps<T>): React.ReactElement {
+  const getter = typeof accessor === 'string' ? R.prop(accessor) : accessor;
+
+  function line(entry: T, index: number) {
+    const text = getter(entry);
+    return <AntTimeline.Item key={`${text}-${index}`}>{text}</AntTimeline.Item>;
+  }
+
+  return <AntTimeline>{items.map(line)}</AntTimeline>;
+}
+
+Timeline.defaultProps = {
+  items: [],
+  accessor: 'title',
+};
+
+Timeline.displayName = 'Timeline';
