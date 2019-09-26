950fda8ff3f0551af94d49997f52a9231a84d785
WhoAteDaCake <doitsimplefy@gmail.com>
2019-09-26 11:31:19 +0100

    (bug) typo

--- packages/ne-graphql/src/helpers/client.ts
+++ packages/ne-graphql/src/helpers/client.ts
@@ -2,7 +2,7 @@ import fetch from 'isomorphic-unfetch';
 import * as R from 'ramda';
 import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
-import { InMemoryCache NormalizedCacheObject } from 'apollo-cache-inmemory';
+import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
 
 const gqlFetch = (uri: RequestInfo, options?: RequestInit) => {
   options.headers = R.mergeRight(options.headers, {
