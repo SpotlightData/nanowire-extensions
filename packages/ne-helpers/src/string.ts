8eba93ffb035bdaa0c362273ef01b89cdb2800ac
WhoAteDaCake <doitsimplefy@gmail.com>
2019-11-27 20:47:32 +0000

    (feat) add levenshteinDistance and  longestStringLen string helpers

--- packages/ne-helpers/src/string.ts
+++ packages/ne-helpers/src/string.ts
@@ -93,3 +93,44 @@ export function hash64(str: string, seed?: number) {
   var h1 = hashFnv32a(str, seed); // returns 32 bit (as 8 byte hex string)
   return h1 + hashFnv32a(h1 + str, seed); // 64 bit (as 16 byte hex string)
 }
+
+// https://github.com/trekhleb/javascript-algorithms/blob/master/src/algorithms/string/levenshtein-distance/levenshteinDistance.js
+export function levenshteinDistance(a: string, b: string): number {
+  // Create empty edit distance matrix for all possible modifications of
+  // substrings of a to substrings of b.
+  const distanceMatrix = Array(b.length + 1)
+    .fill(null)
+    .map(() => Array(a.length + 1).fill(null));
+
+  // Fill the first row of the matrix.
+  // If this is first row then we're transforming empty string to a.
+  // In this case the number of transformations equals to size of a substring.
+  for (let i = 0; i <= a.length; i += 1) {
+    distanceMatrix[0][i] = i;
+  }
+
+  // Fill the first column of the matrix.
+  // If this is first column then we're transforming empty string to b.
+  // In this case the number of transformations equals to size of b substring.
+  for (let j = 0; j <= b.length; j += 1) {
+    distanceMatrix[j][0] = j;
+  }
+
+  for (let j = 1; j <= b.length; j += 1) {
+    for (let i = 1; i <= a.length; i += 1) {
+      const indicator = a[i - 1] === b[j - 1] ? 0 : 1;
+      distanceMatrix[j][i] = Math.min(
+        distanceMatrix[j][i - 1] + 1, // deletion
+        distanceMatrix[j - 1][i] + 1, // insertion
+        distanceMatrix[j - 1][i - 1] + indicator // substitution
+      );
+    }
+  }
+
+  return distanceMatrix[b.length][a.length];
+}
+
+export const longestStringLen = R.pipe(
+  R.map<string, number>(R.prop('length')),
+  R.reduce<number, number>(R.max, 0 as number)
+);
