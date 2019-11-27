65e4e0363c854c8cdc1619252c669afac662ff8f
WhoAteDaCake <doitsimplefy@gmail.com>
2019-11-27 22:47:47 +0000

    (dev) move apollo provider export

--- packages/ne-graphql/src/index.ts
+++ packages/ne-graphql/src/index.ts
@@ -2,3 +2,4 @@ export * from './interfaces';
 export * from './helpers';
 export * from './components';
 export * from './hooks';
+export { ApolloProvider } from 'react-apollo';
--- packages/ne-panel/src/index.ts
+++ packages/ne-panel/src/index.ts
@@ -1,4 +1,3 @@
 export * from './helpers';
 export * from './charts';
 export * from './components';
-export { ApolloProvider } from 'react-apollo';
