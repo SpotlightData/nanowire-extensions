f7f11e5bb6c937bf49cb1b41613e02de69552b02
WhoAteDaCake <doitsimplefy@gmail.com>
2019-11-27 20:59:31 +0000

    (dev) add tests for highlight to marked

--- packages/ne-helpers/tests/text-search.test.ts
+++ packages/ne-helpers/tests/text-search.test.ts
@@ -1,4 +1,4 @@
-import { createMarkedTextRows } from '../src';
+import { createMarkedTextRows, DEFAULT_HIGHLIGHT_MARKER_SYMBOL, highlightToMarked } from '../src';
 
 describe('text-search', () => {
   describe('createMarkedTextRows', () => {
@@ -10,6 +10,26 @@ describe('text-search', () => {
       );
       const saved = output[0].occurrences[0];
       expect(saved.meta.meta).toEqual(meta);
+    });
+  });
+
+  describe('highlightToMarked', () => {
+    it('should work with multiple terms', () => {
+      const text = [
+        DEFAULT_HIGHLIGHT_MARKER_SYMBOL + 'hello' + DEFAULT_HIGHLIGHT_MARKER_SYMBOL,
+        ' sir are you doing ',
+        DEFAULT_HIGHLIGHT_MARKER_SYMBOL,
+        'jquery',
+        DEFAULT_HIGHLIGHT_MARKER_SYMBOL,
+      ].join('');
+      const terms = ['hello', 'jquery'];
+      const marked = highlightToMarked(terms, text);
+      expect(marked).toEqual([
+        { type: 'marked', text: 'hello', start: 0, end: 8 },
+        { type: 'regular', text: ' sir are you doing ', start: 11, end: 30 },
+        { type: 'marked', text: 'jquery', start: 33, end: 39 },
+        { type: 'regular', text: '', start: 39, end: 39 },
+      ]);
     });
   });
 });
