740893a35147910ef093fdcc5ae2cb67b9e9bf36
WhoAteDaCake <doitsimplefy@gmail.com>
2019-11-27 22:13:32 +0000

    (feat) [ne-components] Dropdown updates Export array to dropdown options generator

--- packages/ne-components/src/Dropdown/index.tsx
+++ packages/ne-components/src/Dropdown/index.tsx
@@ -1,45 +1,66 @@
-import React from 'react';
-
+import * as React from 'react';
+import * as R from 'ramda';
 import { Menu, Dropdown as AntDropdown, Button, Icon } from 'antd';
 import { Dictionary } from 'ts-essentials';
+import { DropDownProps as AntDropDownProps } from 'antd/lib/dropdown';
 
 export interface DropdownEntry {
   text: string;
   disabled?: boolean;
 }
 
-export interface DropdownProps {
+type Key = string | number;
+
+export interface DropdownProps<K extends Key> extends Omit<AntDropDownProps, 'overlay'> {
   maxWidth?: number;
-  options: Dictionary<DropdownEntry>;
+  options: Dictionary<DropdownEntry, K>;
   label: string;
-  value: string;
+  value: K | undefined;
   onChange(key: string): void;
 }
 
-export const Dropdown: React.FC<DropdownProps> = ({
+export function arrayToDropdownOptions<K extends Key>(
+  items: K[],
+  map: (item: K) => string
+): Dictionary<DropdownEntry, K> {
+  return R.reduce(
+    (dict, item) => {
+      dict[item] = { text: map(item) };
+      return dict;
+    },
+    {} as Dictionary<DropdownEntry, K>,
+    items
+  );
+}
+
+export function Dropdown<K extends Key>({
   maxWidth,
   options,
   label,
   value,
   onChange,
-}) => {
-  const menu = (
-    <Menu onClick={({ key }) => onChange(key)} style={{ maxWidth: `${maxWidth || 100}px` }}>
-      {Object.keys(options).map(key => (
-        <Menu.Item key={key} disabled={options[key].disabled}>
-          {options[key].text}
-        </Menu.Item>
-      ))}
-    </Menu>
+  ...rest
+}: DropdownProps<K>) {
+  const menu = React.useMemo(
+    () => (
+      <Menu onClick={({ key }) => onChange(key)} style={{ maxWidth }}>
+        {Object.keys(options).map(key => (
+          <Menu.Item key={key} disabled={options[key].disabled}>
+            {options[key].text}
+          </Menu.Item>
+        ))}
+      </Menu>
+    ),
+    [options, onChange, maxWidth]
   );
 
-  const selected = options[value];
+  const selected = value ? options[value] : undefined;
 
   return (
-    <AntDropdown overlay={menu} trigger={['click']}>
+    <AntDropdown overlay={menu} {...rest}>
       <Button className="ant-dropdown-link">
         {selected ? selected.text : label} <Icon type="down" />
       </Button>
     </AntDropdown>
   );
-};
+}
