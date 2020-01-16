753aaddbfe872d21c0b5b13a4084c740308ee89f
WhoAteDaCake <doitsimplefy@gmail.com>
2020-01-16 09:10:07 +0000

    (feat) optional loading class

--- packages/ne-panel/src/components/GraphQLLoadModeRender/index.tsx
+++ packages/ne-panel/src/components/GraphQLLoadModeRender/index.tsx
@@ -4,6 +4,7 @@ import { NotFound, LoadingBox, Loading } from '@spotlightdata/ne-components';
 
 export interface GraphQLLoadModeRenderProps<T> {
   spinner?: 'box' | 'regular';
+  loadingClass?: string;
   mode: AnyGraphQLLoadMode<T>;
   render(data: T, state: AnyGraphQLLoadMode<T>['state']): React.ReactElement;
 }
@@ -12,11 +13,16 @@ export function GraphQLLoadModeRender<T>({
   mode,
   render,
   spinner = 'box',
+  loadingClass,
 }: GraphQLLoadModeRenderProps<T>): React.ReactElement {
   if (mode.state === 'failed') {
     return <GraphQLErrorDisplay errors={mode.errors} />;
   } else if (mode.state === 'loading') {
-    return spinner === 'box' ? <LoadingBox /> : <Loading />;
+    return spinner === 'box' ? (
+      <LoadingBox className={loadingClass} />
+    ) : (
+      <Loading className={loadingClass} />
+    );
   } else if (mode.state === 'not-found') {
     return <NotFound />;
   }
