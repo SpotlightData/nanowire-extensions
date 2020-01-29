b98fe0d1c93df85406c303b13ffa92bdb441f08c
WhoAteDaCake <doitsimplefy@gmail.com>
2020-01-29 15:36:16 +0000

    (feat) [ne-panel] GlumTable

---
+++ packages/ne-panel/src/components/GlumTable/index.tsx
@@ -0,0 +1,61 @@
+import * as React from 'react';
+import { Row } from 'antd';
+import {
+  GraphQLErrorDisplay,
+  GraphQLPagination,
+  GraphQLPaginationDataI,
+  GraphQLLoadUpdateMode,
+} from '@spotlightdata/ne-graphql';
+import { Table } from '@spotlightdata/ne-components';
+import { TableProps, SortOrder } from 'antd/lib/table';
+import { sortEnumOf } from '../../helpers';
+
+export function tableResultsFromGLUM<T>(
+  mode: GraphQLLoadUpdateMode<T[]>
+): { data: T[]; updating: boolean } {
+  let data: T[] = [];
+  if (mode.state === 'updating' || mode.state === 'loaded') {
+    data = mode.data;
+  }
+  return {
+    data,
+    updating: mode.state === 'loading' || mode.state === 'updating',
+  };
+}
+
+export interface GlumTableProps<D> extends TableProps<D> {
+  mode: GraphQLLoadUpdateMode<D[]>;
+  page: GraphQLPaginationDataI;
+  setPage: (nPage: GraphQLPaginationDataI) => void;
+  onSort?: (order: string) => void;
+}
+
+export function GlumTable<D>({
+  mode,
+  page,
+  setPage,
+  onSort,
+  ...rest
+}: GlumTableProps<D>): React.ReactElement {
+  if (mode.state === 'failed') {
+    return <GraphQLErrorDisplay errors={mode.errors} />;
+  }
+  const { data, updating } = tableResultsFromGLUM(mode);
+  return (
+    <React.Fragment>
+      <Table
+        dataSource={data}
+        pagination={false}
+        loading={updating}
+        size="small"
+        onChange={(_pg, _filt, sorter) =>
+          onSort ? onSort(sortEnumOf(sorter.field, sorter.order)) : undefined
+        }
+        {...rest}
+      />
+      <Row type="flex" justify="end" className="margin-top-half">
+        <GraphQLPagination {...page} onChange={setPage} size="small" />
+      </Row>
+    </React.Fragment>
+  );
+}
--- packages/ne-panel/src/components/index.ts
+++ packages/ne-panel/src/components/index.ts
@@ -5,3 +5,4 @@ export * from './SearchMarkerDisplay';
 export * from './IgnoreList';
 export * from './EntityTypes';
 export * from './GraphQLLoadModeRender';
+export * from './GlumTable';
