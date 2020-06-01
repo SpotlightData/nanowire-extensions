911ca96d5de05bc2a63512f00b9108ded099c4c7
WhoAteDaCake <doitsimplefy@gmail.com>
2020-06-01 14:27:56 +0100

    (feat) allow to replace query state

--- packages/ne-components/src/hooks/useQueryTab.ts
+++ packages/ne-components/src/hooks/useQueryTab.ts
@@ -5,13 +5,19 @@ export function useQueryTab(
   initialTab: string,
   allowed: string[],
   tabKey: string = 'tab'
-): [string, (tab: string) => void] {
+): [string, (tab: string, replace?: boolean) => void] {
   const history = useHistory();
   let tab: string = getQuery(history)[tabKey];
   if (!allowed.includes(tab)) {
     tab = initialTab;
   }
 
-  const setTab = (tab: string) => history.push(updateQuery(history, [[tabKey, tab]]).location);
+  const setTab = (tab: string, replace: boolean = true) => {
+    if (replace) {
+      history.push(updateQuery(history, [[tabKey, tab]]).location);
+    } else {
+      history.replace(updateQuery(history, [[tabKey, tab]]).location);
+    }
+  };
   return [tab, setTab];
 }
