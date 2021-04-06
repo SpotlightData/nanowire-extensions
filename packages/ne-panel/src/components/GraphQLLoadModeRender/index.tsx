d344296fd099b30baa6812bb04cbd73fb82799fa
Augustinas <augustinas@spotlightdata.co.uk>
2021-04-06 11:53:46 +0100

    (feat) do not expect not found item

--- packages/ne-panel/src/components/GraphQLLoadModeRender/index.tsx
+++ packages/ne-panel/src/components/GraphQLLoadModeRender/index.tsx
@@ -1,19 +1,25 @@
 import * as React from 'react';
 import { GraphQLErrorDisplay, AnyGraphQLLoadMode } from '@spotlightdata/ne-graphql';
-import { NotFound, LoadingBox, Loading } from '@spotlightdata/ne-components';
+import { LoadingBox, Loading } from '@spotlightdata/ne-components';
 
 export interface GraphQLLoadModeRenderProps<T> {
   spinner?: 'box' | 'regular';
   loadingClass?: string;
   mode: AnyGraphQLLoadMode<T>;
   render(data: T, state: AnyGraphQLLoadMode<T>['state']): React.ReactElement;
+  notFound?: React.ComponentType;
 }
+
+const NotFound = () => {
+  return <div>404 Not found</div>;
+};
 
 export function GraphQLLoadModeRender<T>({
   mode,
   render,
   spinner = 'box',
   loadingClass,
+  notFound = NotFound,
 }: GraphQLLoadModeRenderProps<T>): React.ReactElement {
   if (mode.state === 'failed') {
     return <GraphQLErrorDisplay errors={mode.errors} />;
@@ -24,7 +30,8 @@ export function GraphQLLoadModeRender<T>({
       <Loading className={loadingClass} />
     );
   } else if (mode.state === 'not-found') {
-    return <NotFound />;
+    const Comp = notFound;
+    return <Comp />;
   }
   return render(mode.data, mode.state);
 }
